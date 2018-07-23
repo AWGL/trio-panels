3da3fcd6954948e46da08e0d64d5db4fbbf88ca0
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2018-07-23 12:20:46 +0100

    Add docstrings and comments

--- apply_panel_trios.py
+++ apply_panel_trios.py
@@ -1,7 +1,8 @@
 """
 apply_panel_trios.py
-Author: Erik Waskiewicz
-Date: 26-04-2018
+Author:  Erik Waskiewicz
+Date:    23-07-2018
+Version: 0.1.0
 
 Usage:
 Takes trio variant report txt file outputted from VariantRepoterSpark
@@ -15,42 +16,67 @@ variant report with the panel appended to the file name.
 """
 
 
+## IMPORT PACKAGES ----------------------------------------------------
+
+
 import csv
 import os
 import sys
 import datetime
 
+
+## SET FILEPATHS ------------------------------------------------------
+
+
 BEDTOOLS_FILEPATH = '/Users/erik/Applications/bedtools2/bin/'
 
 
-# inputs - 
+## DEFINE FUNCTIONS ---------------------------------------------------
+
+
 def load_variables():
     '''
-    Takes in sys arg 1 as report filepath
+    Takes in report filepath as argument 1, panel(s) to be applied as
+    argument 2 onwards. Returns arguments, the report filepath, a 
+    string containing the names of all panels applied, and a formatted
+    string that can be used to input the panels into BEDTools programs.
     '''
+
+    # Save and print arguments
     report_input = sys.argv[1]
     print('Report:      ' + str(sys.argv[1]))
     panel_input = sys.argv[2:]
     print('Panel(s):    ' + str(sys.argv[2:]))
-    # bedtool input string
+
+    # Format list of panels for input to BEDTools programs
     bedtools_input = ''
     for panel in panel_input:
         bedtools_input += str(panel + ' ')
-    # extract data from inputs
+
+    # Extract report filepath
     report_path = report_input.rstrip(report_input.split('/')[-1])
+
+    # Make list of input panels
     panel_name = ''
     for panel in panel_input:
         panel_name += panel.split('/')[-1].rstrip('.bed') + '_'
     panel_name = panel_name.rstrip('_')
+
     return(report_input, panel_input, bedtools_input, report_path, panel_name)
 
 
 def make_report_bed(report_input, report_path):
-    # open file to save bed file of variants in report
+    '''
+    Loops through the trio variant report file and converts the genomic
+    coordinates (e.g. 1:123456C>T) and saved as a BED file. The takes
+    this BED file and sorts it using BEDTools sort.
+    '''
+
+    # Open file to save bed file of variants in report
     report_bed_filepath = report_path + 'report_bed.bed'
     report_bed = open(report_bed_filepath, 'w')
-    # extract variants from variant report and make bed file and save 
-    # to file
+
+    # Extract variants from variant report and make bed file
     with open(report_input) as report:
         results = csv.reader(report, delimiter='\t')
         for line in results:
@@ -61,7 +87,8 @@ def make_report_bed(report_input, report_path):
                     + "\t" + variant[1].strip('AGTC>') + "\t"
                     + line[2] + "\n")
     report_bed.close()
-    # sort bed file and save to a different file
+
+    # Sort bed file and save to a different file
     sort_command = BEDTOOLS_FILEPATH + 'sortBed -i ' + report_bed_filepath
     results_sorted = os.popen(sort_command).read()
     report_bed_sorted_filepath = report_path + 'report_bed_sorted.bed'
@@ -71,9 +98,13 @@ def make_report_bed(report_input, report_path):
     os.remove(report_bed_filepath)
 
 
-# extract list of unique variants that intersect both sorted bed file
-# and panel bed file
 def intersect(report_path):
+    '''
+    Extract list of unique variants that intersect both sorted BED file
+    and panel(s) BED file, using BEDTools intersect command.
+    '''
+
+    # Run BEDTools intersect and save results as variable
     report_bed = report_path + 'report_bed_sorted.bed'
     intersect_command = (BEDTOOLS_FILEPATH + 'intersectBed -a ' + report_bed
         + ' -b ' + bedtools_input)
@@ -81,25 +112,38 @@ def intersect(report_path):
         results_intersect = os.popen(intersect_command).read()
     except IOError:
         print("error")
+
+    # Remove empty values
     results_intersect = [
         result for result 
         in results_intersect.split('\n') if result.strip() != ''
         ]
+    
+    # Loop through list and add unique values to new list
     results_intersect_unique = []
     for field in results_intersect:
         if field.split('\t')[3] not in results_intersect_unique:
             results_intersect_unique += [field.split('\t')[3]]
+
     os.remove(report_bed)
     return(results_intersect_unique)
 
 
 def apply_panel(report_input, panel_name, intersect):
-    # loop through the report, compare each variant to intersect list
-    # and keep if they match
+    '''
+    Loop through the trio variant report, compare each variant to the
+    list of variants that intersect with the panel(s) and keep if they
+    match. Save in same folder with the name of the panels(s) appended
+    to the report name.
+    '''
+
+    # Make headeer with panel info
     header = (
         '#Panel applied: ' + str(panel_name) + '. Date: ' 
         + str(datetime.date.today()))
     header_list = [[header]]
+
+    # Loop through report
     output_list = []
     with open(report_input) as report:
         results = csv.reader(report, delimiter='\t')
@@ -109,27 +153,37 @@ def apply_panel(report_input, panel_name, intersect):
             if line[0][0] != '#':
                 if line[2] in intersect:
                     output_list += [line]
-    # open file to save output
+
+    # Open file to save output
     panel_applied_filepath = (
         report_input.split('.')[0] + '_' + panel_name + '.txt'
     )
     panel_applied_output = open(panel_applied_filepath, 'w')
-    # write header and then output to file
+
+    # Write headers and then results to file
     wr = csv.writer(panel_applied_output, delimiter='\t')
     for row in header_list:
         wr.writerow(row)
     for row in output_list:
         wr.writerow(row)
+
     panel_applied_output.close()
     print('Output:      ' + panel_applied_filepath)
 
 
 def filter_denovo(report_input):
-    # loop through the report, add variant if workflow is de novo
+    '''
+    Loop through the trio variant report, keep variant if ther workflow
+    is DE_NOVO. Save in same folder DE_NOVO appended to the report name.
+    '''
+
+    # Make headers
     header = (
         '#Filtered: DE NOVO calls only. Date: ' + str(datetime.datetime.now())
     )
     header_list = [[header]]
+
+    # Loop through the report, add variant if workflow is de novo
     output_list = []
     with open(report_input) as report:
         results = csv.reader(report, delimiter='\t')
@@ -139,25 +193,39 @@ def filter_denovo(report_input):
             if line[0][0] != '#':
                 if 'DE_NOVO' in line[1]:
                     output_list += [line]
-    # open file to save output
+
+    # Open file to save output
     panel_applied_filepath = report_input.split('.')[0] + '_DE_NOVO.txt'
     panel_applied_output = open(panel_applied_filepath, 'w')
-    # write header and then output to file
+
+    # Write headers and then results to file
     wr = csv.writer(panel_applied_output, delimiter='\t')
     for row in header_list:
         wr.writerow(row)
     for row in output_list:
         wr.writerow(row)
+
     panel_applied_output.close()
     print('Output:      ' + panel_applied_filepath)
 
 
-# CALL FUNCTIONS -------------------------------------------------------------
+## CALL FUNCTIONS -----------------------------------------------------
+
+
 print('Applying panels...\n')
+
+# Load variables
 (report_input, panel_input, bedtools_input, report_path, 
     panel_name) = load_variables()
+
+# Turn trio analysis output into BED file
 make_report_bed(report_input, report_path)
+
+# Intersect with panel BED file(s)
 results_intersect_unique = intersect(report_path)
+
+# Apply the panel and filter all de novo calls
 apply_panel(report_input, panel_name, results_intersect_unique)
 filter_denovo(report_input)
+
 print('\nDone.')
--- query_panelapp.py
+++ query_panelapp.py
@@ -1,12 +1,16 @@
 """
 query_panelapp.py
-Author: Erik Waskiewicz
-Date: 19-07-2018
+Author:  Erik Waskiewicz
+Date:    23-07-2018
+Version: 0.1.0
 
 Usage:
 Takes panel unique ID and outputs a bed file of green genes in that
 panel.
 """
+
+
+## IMPORT PACKAGES ----------------------------------------------------
 
 
 import os
@@ -17,41 +21,81 @@ import pandas as pd
 import pybiomart
 import datetime
 
+
+## SET FILEPATHS ------------------------------------------------------
+
+
 BEDTOOLS_FILEPATH = '/Users/erik/Applications/bedtools2/bin/'
 GENOME_FILEPATH = '/Users/erik/Projects/Trio_panelapp/hg19.genome'
+OUTPUT_LOCATION = '/Users/erik/Desktop/panels/'
 
 
-# LIST ALL PANELS
+## DEFINE FUNCTIONS ---------------------------------------------------
+
+
 def list_all_panels():
-    # Extract full list from PanelApp API and store as dataframe
+    '''
+    Extract full list of panels from PanelApp API and store as a pandas
+    dataframe.
+
+    NOTE: NOT CURRENTLY USED - can be used in future to make a tool to
+    select a panel through a GUI, rather than inputting the panel ID
+    through the command line.
+    '''
+
+    # Query PanelApp API
     url = "https://panelapp.genomicsengland.co.uk/WebServices/list_panels/"
     response = requests.get(url)
     data = response.json()
-    #Extract info off all panels within panel app:
+
+    # Extract info of all panels within panel app
     all_panels = pd.io.json.json_normalize(data["result"])
     return(all_panels)
 
 
-# LIST SPECIFIC PANEL INFORMATION
 def get_panel(panel_id):
-# Extract panel information from PanelApp API and save as dataframe
+    '''
+    Input PanelApp panel ID, extract panel information from PanelApp
+    API, split the response into a list of genes and a list of meta
+    data and return both as pandas dataframes.
+    '''
+
+    # Query PanelApp API
     url = ("https://panelapp.genomicsengland.co.uk/WebServices/get_panel/"
      + panel_id + "/")
     panel_response = requests.get(url)
     panel_data = panel_response.json()
+
+    # Filter response for gene list or meta data
     panel_df = pd.io.json.json_normalize(panel_data["result"]["Genes"])
     panel_meta = pd.io.json.json_normalize(panel_data["result"])
     panel_meta = panel_meta.drop(columns=['Genes', 'STRs'])
+
+    # Print summary to screen and return dataframes
     print('Panel:         ' + panel_meta["SpecificDiseaseName"][0])
     print('Version:       ' + panel_meta["version"][0])
     print('PanelApp ID:   ' + panel_id)
+
     return(panel_df, panel_meta)
 
 
 def get_gene_ids(panel_df, green=True, amber=False, red=False):
-    # other option is 'NoList' if numbers dont add up
+    '''
+    Take pandas dataframe of specific panel gene information and filter
+    based on whether each gene is a green, amber or red gene. Return
+    pandas dataframe of filtered genes and an info string to add to the
+    bed file header.
+
+    NOTE: There is another option other than green, amber and red -
+    'NoList'. This might mean that numbers dont add up if comaring to
+    the PanelApp website.
+    '''
+
+    # Make empty variables to collect data
     dfs = []
     info = ''
+
+    # If setting is true, filter dataframe and add to variables
     if green == True:
         green_ids = (panel_df
             [panel_df.LevelOfConfidence == 'HighEvidence']
@@ -59,6 +103,7 @@ def get_gene_ids(panel_df, green=True, amber=False, red=False):
         )
         dfs += [green_ids]
         info += 'Green,'
+
     if amber == True:
         amber_ids = (panel_df
             [panel_df.LevelOfConfidence == 'ModerateEvidence']
@@ -66,6 +111,7 @@ def get_gene_ids(panel_df, green=True, amber=False, red=False):
         )
         dfs += [amber_ids]
         info += 'Amber,'
+
     if red == True:
         red_ids = (panel_df
             [panel_df.LevelOfConfidence == 'LowEvidence']
@@ -73,83 +119,100 @@ def get_gene_ids(panel_df, green=True, amber=False, red=False):
         )
         dfs += [red_ids]
         info += 'Red,'
+
+    # Combine dataframes and remove trailing comma from info
     ids = pd.concat(dfs)
     info = info.rstrip(',')
+
     print('Genes:         ' + info)
     return(ids, info)
 
 
-# SAVE SELECTION AS BED FILE
 def save_as_bed(ensembl_ids, output_location, output_name, header):
-    # biomart - loop through one by one as biomart cant deal with lots
-    # of inputs (not sure what max is - cound speed this up using bigger
-    # batches?)
+    '''
+    Take in pandas dataframe of 1 column containing a list of Ensembl
+    IDs, use the pybiomart package to query each Ensembl ID one by one
+    and return a BED file row for each one. Save the output as a .BED
+    file with a header containing the panel information.
+
+    NOTE: Loop through one by one as biomart cant deal with lots of
+    inputs (not sure what max is - cound speed this up using batches?)
+    '''
+
+    # Load in pybiomart dataset - GRCh37
     dataset = pybiomart.Dataset(
         name='hsapiens_gene_ensembl',
         host='http://grch37.ensembl.org'
     )
 
-    csv_list = []
+    # Query each ID one at a time and add to list
+    bed_list = []
     for gene in ensembl_ids:
         query = dataset.query(
             attributes=['chromosome_name', 'start_position','end_position',
              'external_gene_name'], 
             filters={'link_ensembl_gene_id': gene}
         )
-        csv_list += [query.to_csv(header=False, index=False, sep="\t")]
+        bed_list += [query.to_csv(header=False, index=False, sep="\t")]
 
-    # save csv file
+    # Save BED file
     out = str(output_location) + str(output_name) + '.bed'
     csv_out = open(out, 'w')
     csv_out.write(str(header) + '\n')
-    for item in sorted(csv_list):
+    for item in sorted(bed_list):
         csv_out.write(item)
     csv_out.close()
 
 
 def add_padding(output_location, output_name, header, padding=20):
+    '''
+    Take in BED file and apply padding using BEDTools slop tool,
+    outputs identical BED file with added padding.
+    '''
+
+    # Define inputs and outputs
     input_bed = str(output_location) + str(output_name) + '.bed'
     output_bed_file = (str(output_location) + str(output_name) 
         + '_pad' + str(padding) + '.bed')
+
+    # Run padding command
     sort_command = (BEDTOOLS_FILEPATH + 'slopBed -i ' + input_bed + ' -g '
         + GENOME_FILEPATH + ' -b ' + str(padding))
     padded_bed = os.popen(sort_command).read()
+
+    # Save output
     output_bed = open(output_bed_file, 'w')
-    output_bed.write(str(header) + '\n')
+    output_bed.write(str(header) + '. Padding: ' + str(padding) + 'bp. \n')
     output_bed.write(padded_bed)
     output_bed.close()
     print('Padding:       ' + str(padding))
 
 
-#----------------------------------------------------------------------
-## CALL FUNCTIONS
-
-#all_panels = list_all_panels()
-#print(all_panels.to_string())
-#print(all_panels.DiseaseGroup.unique())
+## CALL FUNCTIONS -----------------------------------------------------
 
 
 print('Downloading panel...\n')
 
-output_location = '/Users/erik/Desktop/panels/'
-#panel = '285'
+# Take panel ID as argument, get panel gene list and meta data
 panel = str(sys.argv[1])
 panel_df, panel_data = get_panel(panel)
 
-# Extract list of Ensemble IDs and level of evidence in PanelApp, store
-#  in dataframe
+# Extract list of Ensemble IDs and filter on level of evidence
 ensembl_ids, genes = get_gene_ids(panel_df)
 
+# Make output filename from variables
 output_name = (panel_data["SpecificDiseaseName"][0].replace(" ", "_") + "_v"
     + panel_data["version"][0] + '_' + genes.replace(',', '_').lower())
 
+# Make BED file header from variables
 header = ('#PanelApp: ' + panel_data["SpecificDiseaseName"][0] + ' v' 
     + panel_data["version"][0] + '. PanelApp ID: ' + panel + '. Genes: ' 
     + genes + '. Group: ' + panel_data["DiseaseGroup"][0] + '. Subgroup: ' 
-    + panel_data["DiseaseSubGroup"][0] + '. Date created ' 
+    + panel_data["DiseaseSubGroup"][0] + '. Date created: ' 
     + str(datetime.date.today()))
 
-save_as_bed(ensembl_ids, output_location, output_name, header)
-add_padding(output_location, output_name, header)
+# Make BED file and add padding
+save_as_bed(ensembl_ids, OUTPUT_LOCATION, output_name, header)
+add_padding(OUTPUT_LOCATION, output_name, header)
 
 print('\nDone.')
